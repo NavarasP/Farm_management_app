4369a43ae5d29672654eb15895e39afa99b2e284
NavarasP <navarasparayil@gmail.com>
2024-03-25 00:08:00 +0530

    Commit

--- lib/authentication/signup/role_selection.dart
+++ lib/authentication/signup/role_selection.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:cluck_connect/authentication/signup/agent_signup.dart';
 import 'package:cluck_connect/authentication/signup/farmer_signup.dart';
 
-
-
 class RoleSelectionScreen extends StatelessWidget {
-  const RoleSelectionScreen({super.key});
+  const RoleSelectionScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,31 +11,35 @@ class RoleSelectionScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Choose Your Role'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const AgentSignUpScreen()),
-                );
-              },
-              child: const Text('I am an Agent'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const FarmerSignUpScreen()),
-                );
-              },
-              child: const Text('I am a Farmer'),
-            ),
-          ],
-        ),
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: [
+          Image.asset(
+            'assets/logo.png',
+            width: 200.0,
+            height: 200.0,
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const AgentSignUpScreen()),
+              );
+            },
+            child: const Text('I am an Agent'),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const FarmerSignUpScreen()),
+              );
+            },
+            child: const Text('I am a Farmer'),
+          ),
+        ],
       ),
     );
   }
